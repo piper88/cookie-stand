8630f804c03ff848df6ada5ebe69ecc893266e5c
Miguel Andrew Aguirre <miguelandrewaguirre@gmail.com>
2016-04-19 17:27:34 -0700

    Fished object constructer for salmon cookies,  with appending to html in a table.

--- js/app.js
+++ js/app.js
@@ -1,215 +1,78 @@
-var PikePlace = {
-  //properties of the object
-  minCustomer: 17,
-  maxCustomer: 88,
-  avgCookiesPerCustomer: 5.2,
-  arrayOfCustomersPerHour: [],
-  arrayOfHourlySales: [],
-  dailyTotal: 0,
-  timeArray: ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm: '],
+var timeArray = ['Location ', '6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm: '];
+var allStores = [];
 
-  //first method takes in min and max number of customers, and returns an array with a random number of customers for each hour of the day
-  calculateHourlyCustomers: function(min, max) {
-    for (var i = 6; i < 21; i++) {
+function Store(name, min, max, averageCookies) {
+  this.name = name;
+  this.min = min;
+  this.max = max;
+  this.averageCookies = averageCookies;
+  this.arrayOfCustomersPerHour = [];
+  this.arrayOfHourlySales = [];
+  this.dailyTotal = 0;
+  this.calculateHourlyCustomer = function (min, max) {
+    for (var i = 0; i < timeArray.length - 1; i++) {
       this.arrayOfCustomersPerHour.push(Math.floor(Math.random() * (max - min + 1) + min));
-    }
-  },
-
-  //second method takes in the array from the first method, and multiplies it by the average cookies per customer, and returns another array that has cookies per hour consumed.
-  calculateHourlySales: function() {
-    this.calculateHourlyCustomers(this.minCustomer, this.maxCustomer);
+    };
+  };
+  this.calculateHourlySales = function () {
+    this.calculateHourlyCustomer(this.min, this.max);
     for (var i = 0; i < this.arrayOfCustomersPerHour.length; i++) {
-      this.arrayOfHourlySales.push(Math.floor(this.arrayOfCustomersPerHour[i] * this.avgCookiesPerCustomer));
+      this.arrayOfHourlySales.push(Math.floor(this.arrayOfCustomersPerHour[i] * this.averageCookies));
       this.dailyTotal += this.arrayOfHourlySales[i];
-    }
-    console.log(this.dailyTotal);
-  },
+    };
+  };
+  console.log(this.dailyTotal);
 
-  renderToPage: function(arraySales) {
-    this.calculateHourlySales();
-    var parentEl = document.getElementById('pikeplace'); //could have outside of for loop
-    for (var i = 0; i < this.arrayOfHourlySales.length; i++) {
-      var listEl = document.createElement('li');
-      listEl.textContent = this.timeArray[i] + this.arrayOfHourlySales[i] + ' cookies';
-      parentEl.appendChild(listEl);
+  this.calculateHourlySales();
+  allStores.push(this);
 
-    } //end of for loop
-    var totalEl = document.createElement('li');
-    totalEl.textContent = 'Total: ' + this.dailyTotal + ' cookies';
-    parentEl.appendChild(totalEl);
-  } //end of rendertoPage method
+  this.renderStore = function () {
+    var tr = document.createElement('tr');
+    var td = document.createElement('td');
+    tr.appendChild(td);
+    td.textContent = this.name;
+    var parentEl = document.getElementById('dataTable');
+    for (i = 0; i < this.arrayOfHourlySales.length; i++){
+      var td = document.createElement('td');
+      td.textContent = this.arrayOfHourlySales[i];
+      tr.appendChild(td);
+    };
+    td.textContent = this.dailyTotal;
+    parentEl.appendChild(tr);
+  };
+  this.renderStore();
+};
+function headerRow () {
+  // var tr = document.createElement('tr');
+  var parentEl = document.getElementById('dataTable');
+  for (i = 0; i < timeArray.length; i++ ){
+    var th = document.createElement('th');
+    th.textContent = timeArray[i];
+    parentEl.appendChild(th);
+  }
+  var totalEl = document.createElement('th');
+  th.textContent = (' Total ');
+  parentEl.appendChild(th);
 };
 
-var Seatac = {
-  //properties of the object
-  minCustomer: 6,
-  maxCustomer: 24,
-  avgCookiesPerCustomer: 1.2,
-  arrayOfCustomersPerHour: [],
-  arrayOfHourlySales: [],
-  dailyTotal: 0,
-  timeArray: ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm: '],
+headerRow ();
+var pike = new Store('PikePlace',17,88,5.2);
+var seaTac = new Store('SeaTac',6,24,1.2);
+var southCenter = new Store('SountCenter',11 ,38,1.9);
+var bellSquare = new Store ('BellSquare',20,48,3.3);
+var alki = new Store ('Alki',3,24,2.6);
 
-  //first method takes in min and max number of customers, and returns an array with a random number of customers for each hour of the day
-  calculateHourlyCustomers: function(min, max) {
-    for (var i = 6; i < 21; i++) {
-      this.arrayOfCustomersPerHour.push(Math.floor(Math.random() * (max - min + 1) + min));
-    }
-  },
+//second method takes in the array from the first method, and multiplies it by the average cookies per customer, and returns another array that has cookies per hour consumed.
 
-  //second method takes in the array from the first method, and multiplies it by the average cookies per customer, and returns another array that has cookies per hour consumed.
-  calculateHourlySales: function() {
-    this.calculateHourlyCustomers(this.minCustomer, this.maxCustomer);
-    for (var i = 0; i < this.arrayOfCustomersPerHour.length; i++) {
-      this.arrayOfHourlySales.push(Math.floor(this.arrayOfCustomersPerHour[i] * this.avgCookiesPerCustomer));
-      this.dailyTotal += this.arrayOfHourlySales[i];
-    }
-    console.log(this.dailyTotal);
-  },
-
-  renderToPage: function(arraySales) {
-    this.calculateHourlySales();
-    var parentEl = document.getElementById('seatac'); //could have outside of for loop
-    for (var i = 0; i < this.arrayOfHourlySales.length; i++) {
-      var listEl = document.createElement('li');
-      listEl.textContent = this.timeArray[i] + this.arrayOfHourlySales[i] + ' cookies';
-      parentEl.appendChild(listEl);
-
-    } //end of for loop
-    var totalEl = document.createElement('li');
-    totalEl.textContent = 'Total: ' + this.dailyTotal + ' cookies';
-    parentEl.appendChild(totalEl);
-  } //end of rendertoPage method
-};
-
-var Southcenter = {
-  //properties of the object
-  minCustomer: 11,
-  maxCustomer: 38,
-  avgCookiesPerCustomer: 1.9,
-  arrayOfCustomersPerHour: [],
-  arrayOfHourlySales: [],
-  dailyTotal: 0,
-  timeArray: ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm: '],
-
-  //first method takes in min and max number of customers, and returns an array with a random number of customers for each hour of the day
-  calculateHourlyCustomers: function(min, max) {
-    for (var i = 6; i < 21; i++) {
-      this.arrayOfCustomersPerHour.push(Math.floor(Math.random() * (max - min + 1) + min));
-    }
-  },
-
-  //second method takes in the array from the first method, and multiplies it by the average cookies per customer, and returns another array that has cookies per hour consumed.
-  calculateHourlySales: function() {
-    this.calculateHourlyCustomers(this.minCustomer, this.maxCustomer);
-    for (var i = 0; i < this.arrayOfCustomersPerHour.length; i++) {
-      this.arrayOfHourlySales.push(Math.floor(this.arrayOfCustomersPerHour[i] * this.avgCookiesPerCustomer));
-      this.dailyTotal += this.arrayOfHourlySales[i];
-    }
-    console.log(this.dailyTotal);
-  },
-
-  renderToPage: function(arraySales) {
-    this.calculateHourlySales();
-    var parentEl = document.getElementById('southcenter'); //could have outside of for loop
-    for (var i = 0; i < this.arrayOfHourlySales.length; i++) {
-      var listEl = document.createElement('li');
-      listEl.textContent = this.timeArray[i] + this.arrayOfHourlySales[i] + ' cookies';
-      parentEl.appendChild(listEl);
-
-    } //end of for loop
-    var totalEl = document.createElement('li');
-    totalEl.textContent = 'Total: ' + this.dailyTotal + ' cookies';
-    parentEl.appendChild(totalEl);
-  } //end of rendertoPage method
-};
-
-var BellevueSquare = {
-  //properties of the object
-  minCustomer: 20,
-  maxCustomer: 48,
-  avgCookiesPerCustomer: 3.3,
-  arrayOfCustomersPerHour: [],
-  arrayOfHourlySales: [],
-  dailyTotal: 0,
-  timeArray: ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm: '],
-
-  //first method takes in min and max number of customers, and returns an array with a random number of customers for each hour of the day
-  calculateHourlyCustomers: function(min, max) {
-    for (var i = 6; i < 21; i++) {
-      this.arrayOfCustomersPerHour.push(Math.floor(Math.random() * (max - min + 1) + min));
-    }
-  },
-
-  //second method takes in the array from the first method, and multiplies it by the average cookies per customer, and returns another array that has cookies per hour consumed.
-  calculateHourlySales: function() {
-    this.calculateHourlyCustomers(this.minCustomer, this.maxCustomer);
-    for (var i = 0; i < this.arrayOfCustomersPerHour.length; i++) {
-      this.arrayOfHourlySales.push(Math.floor(this.arrayOfCustomersPerHour[i] * this.avgCookiesPerCustomer));
-      this.dailyTotal += this.arrayOfHourlySales[i];
-    }
-    console.log(this.dailyTotal);
-  },
-
-  renderToPage: function(arraySales) {
-    this.calculateHourlySales();
-    var parentEl = document.getElementById('bellevue'); //could have outside of for loop
-    for (var i = 0; i < this.arrayOfHourlySales.length; i++) {
-      var listEl = document.createElement('li');
-      listEl.textContent = this.timeArray[i] + this.arrayOfHourlySales[i] + ' cookies';
-      parentEl.appendChild(listEl);
-
-    } //end of for loop
-    var totalEl = document.createElement('li');
-    totalEl.textContent = 'Total: ' + this.dailyTotal + ' cookies';
-    parentEl.appendChild(totalEl);
-  } //end of rendertoPage method
-};
-
-var Alki = {
-  //properties of the object
-  minCustomer: 3,
-  maxCustomer: 24,
-  avgCookiesPerCustomer: 2.6,
-  arrayOfCustomersPerHour: [],
-  arrayOfHourlySales: [],
-  dailyTotal: 0,
-  timeArray: ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm: '],
-
-  //first method takes in min and max number of customers, and returns an array with a random number of customers for each hour of the day
-  calculateHourlyCustomers: function(min, max) {
-    for (var i = 6; i < 21; i++) {
-      this.arrayOfCustomersPerHour.push(Math.floor(Math.random() * (max - min + 1) + min));
-    }
-  },
-
-  //second method takes in the array from the first method, and multiplies it by the average cookies per customer, and returns another array that has cookies per hour consumed.
-  calculateHourlySales: function() {
-    this.calculateHourlyCustomers(this.minCustomer, this.maxCustomer);
-    for (var i = 0; i < this.arrayOfCustomersPerHour.length; i++) {
-      this.arrayOfHourlySales.push(Math.floor(this.arrayOfCustomersPerHour[i] * this.avgCookiesPerCustomer));
-      this.dailyTotal += this.arrayOfHourlySales[i];
-    }
-    console.log(this.dailyTotal);
-  },
-
-  renderToPage: function(arraySales) {
-    this.calculateHourlySales();
-    var parentEl = document.getElementById('alki'); //could have outside of for loop
-    for (var i = 0; i < this.arrayOfHourlySales.length; i++) {
-      var listEl = document.createElement('li');
-      listEl.textContent = this.timeArray[i] + this.arrayOfHourlySales[i] + ' cookies';
-      parentEl.appendChild(listEl);
-
-    } //end of for loop
-    var totalEl = document.createElement('li');
-    totalEl.textContent = 'Total: ' + this.dailyTotal + ' cookies';
-    parentEl.appendChild(totalEl);
-  } //end of rendertoPage method
-};
-
-PikePlace.renderToPage();
-Seatac.renderToPage();
-Southcenter.renderToPage();
-BellevueSquare.renderToPage();
-Alki.renderToPage();
+// function renderToPage (arraySales) {
+// this.calculateHourlySales();
+//   var parentEl = document.getElementById('alki'); //could have outside of for loop
+//   for (var i = 0; i < this.arrayOfHourlySales.length; i++) {
+//     var listEl = document.createElement('li');
+//     listEl.textContent = this.timeArray[i] + this.arrayOfHourlySales[i] + ' cookies';
+//     parentEl.appendChild(listEl);
+//   }; //end of for loop
+//   var totalEl = document.createElement('li');
+//   totalEl.textContent = 'Total: ' + this.dailyTotal + ' cookies';
+//   parentEl.appendChild(totalEl);
+// } //end of rendertoPage method
